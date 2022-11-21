993d6e4ae56e599be2332cb48b878d661405d397
Vladimir Golub <golubvlauri@gmail.com>
2022-11-21 23:24:33 +0200

    add articles recolver

---
+++ backend/src/articles/resolvers/articles.recolver.ts
@@ -0,0 +1,30 @@
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { ArticleModel } from '../models/article.model';
+import { ArticlesService } from '../services/articles.service';
+import { NotFoundException } from '@nestjs/common';
+import { ArticlesArgs } from '../dto/articles.args';
+
+@Resolver(of => ArticleModel)
+export class ArticlesRecolver {
+
+  constructor(
+    private readonly articlesService: ArticlesService
+  ) {}
+
+  @Query(returns => ArticleModel)
+  async getArticleByID(@Args('id') id: string): Promise<ArticleModel> {
+    const article = await this.articlesService.findOneByID(id);
+
+    if (!article) {
+      throw new NotFoundException(id);
+    }
+
+    return article;
+  }
+
+  @Query(returns => Array<ArticleModel>)
+  async getArticlesAll(@Args() articlesArgs: ArticlesArgs): Promise<ArticleModel[]> {
+    return await this.articlesService.findAll(ArticlesArgs)
+  }
+
+}
